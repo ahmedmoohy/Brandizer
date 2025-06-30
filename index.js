5d787dab3b829854fbf6ba62b9c1c0623de21370
ahmedmoohy <ahmedmohyenglevel1@gmail.com>
2025-06-30 12:21:54 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Brandizer</title>
+      </Head>
+      <main>
+        <h1>Welcome to Brandizer 👋</h1>
+      </main>
+    </>
+  )
+}
